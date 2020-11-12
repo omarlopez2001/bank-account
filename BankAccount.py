bffa2bf1d053e2646719eaf172858b7f2d53c118
omarlopez2001 <omarlopez9445@gmail.com>
2020-11-11 23:34:29 -0500

    Fixed mistakes
    I went back to add and fix certain areas of my code to match the assignment requirements.

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,6 @@
 import re
+import random
 class BankAccount:
-    balance = 0
     def __init__(self, full_name, account_number, routing_number, balance):
         self.name = full_name
         self.account = account_number
@@ -9,8 +9,8 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += amount
-        print(f'Amount Deposited: {amount}')
-        return (f'Account balance: {self.balance}')
+        print(f'Amount Deposited: ${amount}')
+        return (f'Account balance: ${self.balance}')
     
     def withdraw(self, amount):
         self.balance = self.balance - amount
@@ -18,17 +18,17 @@ class BankAccount:
             self.balance = self.balance - 10
             print(f'Insufficient funds. You have been charged an overdraft fee of $10.')
         elif self.balance > amount:
-            print(f'Amount Withdrawn: {amount}')
-        return (f'Account balance: {self.balance}')
+            print(f'Amount Withdrawn: ${amount}')
+        return (f'Account balance: ${self.balance}')
 
     def get_balance(self):
-        print(f'Your account balance is: {self.balance}')
+        print(f'Your account balance is: ${self.balance}')
         return self.balance
 
     def add_interest(self):
         interest = self.balance * 0.00083
         self.balance += interest
-        return self.balance
+        return (f'After your monthly interest: ${self.balance}')
     
     def print_receipt(self):
         print(self.name)
@@ -37,9 +37,9 @@ class BankAccount:
         print(f'Routing No.: {self.routing}')
         print(f'Balance: ${self.balance}')
 
-omar = BankAccount('Omar Lopez', 20988763, 53981284, 890)
-abel = BankAccount('Abel Pineda', 34506398, 61039044, 900)
-edgar = BankAccount('Edgar Ulloa', 89680089, 99225678, 670)
+omar = BankAccount('Omar Lopez', random.randint(10000000,99999999), 123456789, 0)
+abel = BankAccount('Abel Pineda', random.randint(10000000,99999999), 123456789, 0)
+edgar = BankAccount('Edgar Ulloa', random.randint(10000000,99999999), 123456789, 0)
 
 print(f'Welcome to Terminal ATM!')
 print(f'Please enter your first name.')
@@ -47,63 +47,76 @@ print(f'Please enter your first name.')
 user = ""
 
 user = input('What is your first name?:')
-if user == 'omar':
-    choice = input(""" Please choose from the following options: 
-    1. Deposit money into account
-    2. Withdraw money from account
-    3. View balance
-    4. View monthly interest
-    5. Print out receipt
-    """)
-    if choice == "1":
-        amount = int(input('How much money would you like to deposit?'))
-        print(omar.deposit(amount))
-    elif choice == "2":
-        amount = int(input('How much money would you like to withdraw?'))
-        print(omar.withdraw(amount))
-    elif choice == "3":
-        print(omar.get_balance())
-    elif choice == "4":
-        print(omar.add_interest())
-    elif choice == "5":
-        print(omar.print_receipt())
-elif user == 'abel':
-    choice = input(""" Please choose from the following options: 
-    1. Deposit money into account
-    2. Withdraw money from account
-    3. View balance
-    4. View monthly interest
-    5. Print out receipt
-    """)
-    if choice == "1":
-        amount = int(input('How much money would you like to deposit?'))
-        print(abel.deposit(amount))
-    elif choice == "2":
-        amount = int(input('How much money would you like to withdraw?'))
-        print(abel.withdraw(amount))
-    elif choice == "3":
-        print(abel.get_balance())
-    elif choice == "4":
-        print(abel.add_interest())
-    elif choice == "5":
-        print(abel.print_receipt())
-elif user == 'edgar':
-    choice = input(""" Please choose from the following options: 
-    1. Deposit money into account
-    2. Withdraw money from account
-    3. View balance
-    4. View monthly interest
-    5. Print out receipt
-    """)
-    if choice == "1":
-        amount = int(input('How much money would you like to deposit?'))
-        print(edgar.deposit(amount))
-    elif choice == "2":
-        amount = int(input('How much money would you like to withdraw?'))
-        print(edgar.withdraw(amount))
-    elif choice == "3":
-        print(edgar.get_balance())
-    elif choice == "4":
-        print(edgar.add_interest())
-    elif choice == "5":
-        print(edgar.print_receipt())
+while True:
+    if user == 'omar':
+        choice = input(""" Please choose from the following options: 
+        1. Deposit money into account
+        2. Withdraw money from account
+        3. View balance
+        4. View monthly interest
+        5. Print out receipt
+        6. Done
+        """)
+        if choice == "1":
+            amount = int(input('How much money would you like to deposit?'))
+            print(omar.deposit(amount))
+        elif choice == "2":
+            amount = int(input('How much money would you like to withdraw?'))
+            print(omar.withdraw(amount))
+        elif choice == "3":
+            print(omar.get_balance())
+        elif choice == "4":
+            print(omar.add_interest())
+        elif choice == "5":
+            print(omar.print_receipt())
+        elif choice == "6":
+            print(f'Thank you for using Terminal ATM, enjoy your day!')
+            break
+    elif user == 'abel':
+        choice = input(""" Please choose from the following options: 
+        1. Deposit money into account
+        2. Withdraw money from account
+        3. View balance
+        4. View monthly interest
+        5. Print out receipt
+        6. Done
+        """)
+        if choice == "1":
+            amount = int(input('How much money would you like to deposit?'))
+            print(abel.deposit(amount))
+        elif choice == "2":
+            amount = int(input('How much money would you like to withdraw?'))
+            print(abel.withdraw(amount))
+        elif choice == "3":
+            print(abel.get_balance())
+        elif choice == "4":
+            print(abel.add_interest())
+        elif choice == "5":
+            print(abel.print_receipt())
+        elif choice == "6":
+            print(f'Thank you for using Terminal ATM, enjoy your day!')
+            break
+    elif user == 'edgar':
+        choice = input(""" Please choose from the following options: 
+        1. Deposit money into account
+        2. Withdraw money from account
+        3. View balance
+        4. View monthly interest
+        5. Print out receipt
+        6. Done
+        """)
+        if choice == "1":
+            amount = int(input('How much money would you like to deposit?'))
+            print(edgar.deposit(amount))
+        elif choice == "2":
+            amount = int(input('How much money would you like to withdraw?'))
+            print(edgar.withdraw(amount))
+        elif choice == "3":
+            print(edgar.get_balance())
+        elif choice == "4":
+            print(edgar.add_interest())
+        elif choice == "5":
+            print(edgar.print_receipt())
+        elif choice == "6":
+            print(f'Thank you for using Terminal ATM, enjoy your day!')
+            break
--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Project title: Bank Account
 Description: I created a Python program that simulates a bank account.
 Programming language used: Python 3.8.3
 
-For print_receipt method, I used https://stackoverflow.com/questions/19084443/replacing-digits-with-str-replace to learn how to sensitize the first four digits of the account number with asterisks.
+For print_receipt method, I used https://stackoverflow.com/questions/19084443/replacing-digits-with-str-replace to learn how to sensitize the first four digits of the account number with asterisks. 
+To randomly generate the eight digits for the account number, I used random.randint(10000000,99999999), which I learned from https://www.kite.com/python/answers/how-to-generate-a-random-number-of-length-n-in-python and https://stackoverflow.com/questions/32050155/how-to-generate-multiple-9-digit-random-numbers-in-one-go-and-add-a-prefix-numbe
